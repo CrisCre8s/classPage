4d394621b629d9e707b58915975e2a71da34ed6d
cotte <cotte@skatedeluxe.com>
2026-03-21 16:10:54 +0100

    deleting react, make it more simple

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
+<<<<<<< HEAD
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
 });
+=======
+import { defineConfig } from "vite";
+
+export default defineConfig({});
+>>>>>>> 24170e0 (deleting react, make it more simple)
